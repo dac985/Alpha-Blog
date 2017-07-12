2c9b75b89aedaae5493f42a4b4fba7c8c9d70c96
Derek Cantrell <dac985@gmail.com>
2017-07-12 02:49:08 +0000

    Create rails application and setup home and about pages

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  
+  def home
+    
+  end
+  
+  def about
+    
+  end
+  
+  def alphablog_home
+    
+  end
+  
+  def alphablog_about
+    
+  end
+  
+end
